d6969f9125153d46869ac7a41298e89a28b0e703
AzmanThoughtWin <azman.thoughtwin@gmail.com>
2021-08-27 15:43:58 +0530

    Conditionally runs useEffect

--- src/components/ClassCounterOne.js
+++ src/components/ClassCounterOne.js
@@ -7,6 +7,7 @@ class ClassCounterOne extends Component {
     
         this.state = {
             count : 0,
+            name : ''
         }
     }
     
@@ -15,12 +16,20 @@ class ClassCounterOne extends Component {
     }
     
     componentDidUpdate(prevProps, prevState) {
-        document.title = `Clicked ${this.state.count} times`
+        if(prevState.count !== this.state.count){
+            console.log("Updating document title")
+            document.title = `Clicked ${this.state.count} times`
+        }
     }
 
     render() {
         return (
             <div>
+                <input
+                    type="text"
+                    value={this.state.name}
+                    onChange={ e => this.setState({ name : e.target.value }) }
+                />
                 <button onClick={() => this.setState({ count : this.state.count + 1 })}>
                     Click {this.state.count} times
                 </button>
