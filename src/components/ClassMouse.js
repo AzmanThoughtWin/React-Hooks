11ad172d4a06c2e4295e4a5062048ee84642f838
AzmanThoughtWin <azman.thoughtwin@gmail.com>
2021-08-27 17:23:04 +0530

    useEffect with cleanup

--- src/components/ClassMouse.js
+++ src/components/ClassMouse.js
@@ -19,6 +19,10 @@ class ClassMouse extends Component {
         window.addEventListener('mousemove', this.logMousePosition)
     }
 
+    componentWillUnmount() {
+        window.removeEventListener('mousemove', this.logMousePosition)
+    }
+
     render() {
         return (
             <div>
