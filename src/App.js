cabc7766358a3e1dfc1ae6e15ff9e58f4a62cf14
AzmanThoughtWin <azman.thoughtwin@gmail.com>
2021-08-30 18:19:57 +0530

    Example of useCallback

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import DataFetchingOne from './components/DataFetchingOne';
-import DataFetchingTwo from './components/DataFetchingTwo';
+import ParentComponent from './components/ParentComponent'
 
 function App() {
   return (
       <div className="App">
-        {/* <DataFetchingOne/> */}
-        <DataFetchingTwo/>
+        <ParentComponent/>
       </div>
   );
 }
